3798a5c01051fbdce9e6802de2298b0c9218ce0c
Oskari Ojamaa <eeosoj@utu.fi>
2024-01-11 21:07:13 +0200

    Add collision resolution

--- PointMass.cs
+++ PointMass.cs
@@ -1,10 +1,15 @@
 using System.Numerics;
+using Collision;
 using static Raylib_cs.Raylib;
 
 namespace Physics;
 
 public class PointMass
 {
+    public const float RestitutionCoeff = 0.95f;
+    public const float FrictionCoeff = 0.1f;
+    public const float BounceThreshold = 1e-4f;
+
     public Vector2 Pos { get; set; }
     public Vector2 PrevPos { get; set; }
     public Vector2 Acc { get; set; }
@@ -20,17 +25,18 @@ public class PointMass
     {
         Pos = new(x, y);
         PrevPos = Pos;
-        Acc = new();
+        Acc = Vector2.Zero;
         Mass = mass;
         Radius = mass * 5f;
     }
 
-    public void Update()
+    public void Update(in List<LineCollider> lineColliders)
     {
         if (Sim.Loop.GravityEnabled)
         {
             Acc += Sim.Loop.Gravity;
         }
+        SolveCollisions(lineColliders);
         Vector2 vel = Vel;
         PrevPos = Pos;
         Pos += vel + Acc * Sim.Loop.TimeStep * Sim.Loop.TimeStep;
@@ -42,8 +48,40 @@ public class PointMass
         DrawCircleLines((int) Pos.X, (int) Pos.Y, Radius, Raylib_cs.Color.WHITE);
     }
 
-    public void ApplyForce(Vector2 f)
+    public void ApplyForce(in Vector2 f)
     {
         Acc += f / Mass;
+    }
+
+    public void SolveCollisions(in List<LineCollider> lineColliders)
+    {
+        foreach (LineCollider c in lineColliders)
+        {
+            Vector2 closestPoint = c.ClosestPointOnLine(Pos);
+            Vector2 closestToPoint = Pos - closestPoint;
+            float distToCollider = closestToPoint.Length();
+            if (distToCollider <= Radius)
+            {
+                // Collision
+                Vector2 closestToPointNorm = Vector2.Normalize(closestToPoint);
+                Vector2 reflectedVel = Vel - closestToPointNorm * 2f * Vector2.Dot(Vel, closestToPointNorm);
+                Pos += (Radius - distToCollider) * closestToPointNorm;
+                if (Vel.Length() < BounceThreshold)
+                {
+                    continue;
+                }
+                Vel = RestitutionCoeff * reflectedVel;
+                ApplyKineticFriction(closestToPointNorm);
+            }
+        }
+    }
+
+    private void ApplyKineticFriction(in Vector2 normal)
+    {
+        Vector2 dirA = new(normal.Y, normal.X);
+        Vector2 dirB = new(-normal.Y, normal.X);
+        Vector2 dir = Vector2.Dot(Vel, dirA) < 0f ? dirA : dirB;
+        Vector2 frictionF = dir * FrictionCoeff * Mass * Math.Abs(Vector2.Dot(Acc, normal));
+        ApplyForce(frictionF);
     }
 }
--- SimLoop.cs
+++ SimLoop.cs
@@ -8,41 +8,58 @@ namespace Sim;
 public class Loop 
 {   
     public const float TimeStep = 1f / 60f;
-    public const int WinW = 800;
-    public const int WinH = 450;
+    public const int WinW = 1600;
+    public const int WinH = 900;
     public const float PixelsPerMeter = 1f / 0.01f;
 
     public static bool GravityEnabled { get; set; }
     public static readonly Vector2 Gravity = new(0f, 9.81f * PixelsPerMeter);
 
     private static float _accumulator;
+    private static List<LineCollider> _lineColliders;
 
     public static void Main() 
     {
-        InitWindow(WinW, WinH, "Point-masses");
+        InitWindow(WinW, WinH, "");
         SetTargetFPS(165);
 
-        PointMass mass = new(WinW / 2f, WinH / 2f, 10f);
-        LineCollider line = new(0f, 300f, 800f, 450f);
+        PointMass mass = new(1500f, 730f, 10f);
+        _lineColliders = new() {
+            new(0f, 0f, WinW, 0f),
+            new(0f, 0f, 0f, WinH),
+            new(WinW, 0f, WinW, WinH),
+            new(0f, 900f, 1600f, 780f) 
+        };
         GravityEnabled = true;
 
         while (!WindowShouldClose())
         {
+            SetWindowTitle(string.Format("Point-masses FPS: {0}", GetFPS()));
             _accumulator += GetFrameTime();
             while (_accumulator >= TimeStep)
             {
-                mass.Update();
+                mass.Update(_lineColliders);
                 _accumulator -= TimeStep;
             }
-            Vector2 closestPoint = line.ClosestPointOnLine(mass.Pos);
+            HandleInput();
             BeginDrawing();
             ClearBackground(Raylib_cs.Color.BLACK);
             mass.Draw();
-            line.Draw();
-            DrawLine((int) closestPoint.X, (int) closestPoint.Y, (int) mass.Pos.X, (int) mass.Pos.Y, Raylib_cs.Color.MAGENTA);
+            foreach (var c in _lineColliders)
+            {
+                c.Draw();
+            }
             EndDrawing();
         }
 
         CloseWindow();
+    }
+
+    private static void HandleInput()
+    {
+        if (IsKeyPressed(Raylib_cs.KeyboardKey.KEY_G))
+        {
+            GravityEnabled = !GravityEnabled;
+        }
     }
 }
